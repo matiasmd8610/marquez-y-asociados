613f2b903c489eb60eab35781e046e81e37a0f13
Matias Di Iorio <matiasmd8610@gmail.com>
2021-08-25 17:38:11 -0300

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+let buttonsToForm = document.getElementsByClassName("btn");
+
+for (let item of buttonsToForm) {
+    item.addEventListener('click', (e)=> {
+        let target = document.querySelector("#section-form");
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        e.preventDefault();
+    })
+}
